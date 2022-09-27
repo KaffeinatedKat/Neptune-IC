c3ddfca62bfd799f521e60b487bc4c684dee4d1c
John Crawford <coffeeboi47@protonmail.com>
2022-09-27 11:18:37 -0600

    refactor: src directory

---
+++ src/help_text.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+void profileAllHelp() {
+    printf("<Profile Help>\n\n"
+"Profiles are how you login to your different infinite campus accounts\n"
+"For information on how to create profiles, refer to the Neptune-IC\n"
+"github page at https://github.com/KaffeinatedKat/Neptune-IC\n\n");
+}
