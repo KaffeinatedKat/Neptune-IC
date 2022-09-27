0144504cb6c8f3d1404c0a671d24fad310c60425
John Crawford <coffeeboi47@protonmail.com>
2022-09-27 11:20:04 -0600

    refactor: forgot to remove old files lmao

--- help_text.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-void profileAllHelp() {
-    printf("<Profile Help>\n\n"
-"Profiles are how you login to your different infinite campus accounts\n"
-"For information on how to create profiles, refer to the Neptune-IC\n"
-"github page at https://github.com/KaffeinatedKat/Neptune-IC\n\n");
-}
