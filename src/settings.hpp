c6c967f4125cde1f0073699989ff065018967318
John Crawford <coffeeboi47@protonmail.com>
2022-11-15 12:08:28 -0700

    refactor: some code cleanup

--- src/settings.hpp
+++ src/settings.hpp
@@ -7,29 +7,21 @@ using json = nlohmann::json;
 
 struct Options {
     json settings_json;
-    bool colors = true;
-    std::string missing_color;
-    std::string color_reset;
-    int default_notification_count;
+    bool colors = false;
+    std::string missing_color = "";
+    std::string color_reset = "";
+    int default_notification_count = 5;
 
     Options load(Options Settings) {
         std::ifstream f("../settings.json"); //  Open file, load json into settings_json, then close the file
         Settings.settings_json = json::parse(f);
         f.close();
 
-        try {
-            Settings.default_notification_count = Settings.settings_json[0]["notification-count"]; //  Try settings notification count
-        } catch (nlohmann::detail::type_error) {
-            Settings.default_notification_count = 5; //  If value is absent, default is 5
-        }
+        Settings.default_notification_count = Settings.settings_json[0]["notification-count"]; //  Set default notification count
 
-        if (Settings.settings_json[0]["colored-text"]["enabled"] == false) { //  Completely disable ascii escape codes is color setting is disabled, for terminals that do not support it
-            Settings.colors = false;
-            Settings.missing_color = "";  
-            Settings.color_reset = "";
-        } else {
+        if (Settings.settings_json[0]["colored-text"]["enabled"] == true) { //  Enable terminal colors
             Settings.colors = true;
-            Settings.missing_color = "\033[1;31m";
+            Settings.missing_color = "\033[1;31m";  
             Settings.color_reset = "\033[0m";
         }
 
@@ -41,7 +33,7 @@ struct Options {
             json colors = Settings.settings_json[0]["colored-text"];
             auto it = colors["grade-colors"].find(grade);
             
-            if (it == colors["grade-colors"].end() || colors["enabled"] == false) { return ""; }
+            if (it == colors["grade-colors"].end() || Settings.colors == false) { return ""; }
 
             return "\033[" + std::string(it.value()) + "m";
     }
